1f8d6c89ec0c94724b0ed2604ef1d0819b898cee
Mike Patrick <mpatrick76@gmail.com>
2017-12-30 08:17:18 -0600

    Add code.

---
+++ src/main/java/com/mike/patrick/mvntest/MvntestApplication.java
@@ -0,0 +1,12 @@
+package com.mike.patrick.mvntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvntestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvntestApplication.class, args);
+	}
+}
